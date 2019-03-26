442a051fd6160bb21fc81b143306624f56997e24
ksvkabra@gmail.com <ksvkabra@gmail.com>
2019-03-27 01:27:55 +0530

    react animations

--- src/components/DishComponent.js
+++ src/components/DishComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
     function RenderComment({comments}) {
         if(comments !=null){
@@ -11,14 +12,18 @@ import { baseUrl } from '../shared/baseUrl';
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {comments.map((comment) => {
-                            return(
-                                <li key={comment.id}>
-                                    <div>{comment.comment}</div>
-                                    <div>-- {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
-                                </li>
-                            );
-                        })}
+                        <Stagger in>
+                            {comments.map((comment) => {
+                                return(
+                                    <Fade in>
+                                        <li key={comment.id}>
+                                            <div>{comment.comment}</div>
+                                            <div>-- {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
+                                        </li>
+                                    </Fade>
+                                );
+                            })}
+                        </Stagger>
                     </ul>
                 </div>
             );
@@ -28,14 +33,21 @@ import { baseUrl } from '../shared/baseUrl';
 
     function RenderDish({dish}) {
         return (
-            <div className="col-12 ccol-md-5 m-1">
-                <Card body className="text-left" >
-                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+            <div className="col-12 col-md-5 m-1">
+                <FadeTransform in
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}
+                >
+                    <Card body className="text-left" >
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+
+                </FadeTransform>
             </div>
         );
     }
